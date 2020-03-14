b8cb1e154aba0060e87b4816af23c5df7183b603
jacob <jacob1107@163.com>
2020-03-14 22:26:36 +0800

    feign 提交

--- cloud-consumer-feign-order80/src/main/java/com/jacob/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/jacob/springcloud/controller/OrderFeignController.java
@@ -1,8 +1,8 @@
-package com.atguigu.springcloud.controller;
+package com.jacob.springcloud.controller;
 
-import com.atguigu.springcloud.service.PaymentFeignService;
 import com.jacob.springcloud.entities.CommonResult;
 import com.jacob.springcloud.entities.Payment;
+import com.jacob.springcloud.service.PaymentFeignService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
--- cloud-consumer-feign-order80/src/main/java/com/jacob/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/jacob/springcloud/service/PaymentFeignService.java
@@ -1,11 +1,9 @@
-package com.atguigu.springcloud.service;
+package com.jacob.springcloud.service;
 
-import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import com.jacob.springcloud.entities.CommonResult;
 import com.jacob.springcloud.entities.Payment;
 
@@ -15,11 +13,10 @@ import com.jacob.springcloud.entities.Payment;
  */
 @Component
 @FeignClient(value = "CLOUD-PAYMENT-SERVICE")
-public interface PaymentFeignService
-{
-    @GetMapping(value = "/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+public interface PaymentFeignService {
+	@GetMapping(value = "/payment/get/{id}")
+	public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
 
-    @GetMapping(value = "/payment/feign/timeout")
-    public String paymentFeignTimeout();
+	@GetMapping(value = "/payment/feign/timeout")
+	public String paymentFeignTimeout();
 }
