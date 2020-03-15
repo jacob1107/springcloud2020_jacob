ef2885f01cd6aea83c3aad2bcdb7d447d3d7811a
jacob <jacob1107@163.com>
2020-03-15 18:02:53 +0800

    nacos 提交

---
+++ cloudalibaba-config-nacos-client3377/src/main/resources/nacos.md
@@ -0,0 +1,2 @@
+http://127.0.0.1:8848/nacos/#/login
+nacos-config-client-dev.yaml
---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/jacob/springcloud/alibaba/service/PaymentService.java
@@ -0,0 +1,19 @@
+package com.jacob.springcloud.alibaba.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.jacob.springcloud.entities.CommonResult;
+import com.jacob.springcloud.entities.Payment;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 18:15
+ */
+@FeignClient(value = "nacos-payment-provider",fallback = PaymentFallbackService.class)
+public interface PaymentService
+{
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
